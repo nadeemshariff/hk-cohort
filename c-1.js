8ecfa8484143b087dd79e7930a4b1359fc4a4fe0
nadeemshariff <108820002+nadeemshariff@users.noreply.github.com>
2024-06-03 20:42:04 +0530

    check list

--- c-1.js
+++ c-1.js
@@ -41,11 +41,79 @@
 
 // console.log(personArray)
 
-const ages = [21, 22, 23, 24, 25];
-const numberOfPeople = ages.length;
+// write a program to print all the even numbers in an array
 
-for (let i = 0; i < numberOfPeople; i++) {
-    if (ages[i] % 2 == 0) {
-        console.log(ages[i]); 
-    }
-}
+// const ages = [21, 22, 23, 24, 25];
+// const numberOfPeople = ages.length;
+
+// for (let i = 0; i < numberOfPeople; i++) {
+//     if (ages[i] % 2 == 0) {
+//         console.log(ages[i]); 
+//     }
+// }
+
+//write a program to print a biggest number in a array
+
+// const num = [10, 15, 21, 25, 22, 29, 35, 32];
+
+// for (let i = 0; i < num.length; i++) {
+//     if (num[i] > num[i + 1]) {
+//     console.log(num[i]) ;
+//     }
+// }
+
+//write a program to print a smallest number in a array
+
+// const num = [10, 15, 21, 25, 22, 29, 35, 32];
+
+// for (let i = 0; i < num.length; i++) {
+//     if (num[i] < num[i + 1]) {
+//     console.log(num[i]) ;
+//     }
+// }
+
+
+// write a program that prints all the male peoples first name given a complex object.
+
+// const p1 = "name1";
+// const g1 = "male";
+
+// const p2 = "name2";
+// const g2 = "male";
+
+// const p3 = "name3";
+// const g3 = "female";
+
+// const pArray = ["name1", "'name2", "name3"];
+
+// const gArray = ["male", "male", "female"];
+
+// const users = [{
+//     name: "Nadeem",
+//     gender: "male",
+//     metadata: {
+//         age: 25,
+//         city: ""
+//     }
+// },{
+//     name: "Shariff",
+//     gender: "male"
+// },{
+//     name: "Mansha",
+//     gender: "female"
+// },{
+//     name: "Sara",
+//     gender: "female"
+// }];
+
+// for (let i = 0; i < users.length; i++) {
+//     if (users[i]["gender"] == "male"){
+//         console.log(users[i]["name"]);
+//     }
+// }
+
+// for (let i = 0; i < pArray.length; i++) {
+//     if (gArray[i] == "male") {
+//         console.log(pArray[i]);
+//     }
+// };
---
+++ c-2.js
@@ -0,0 +1,31 @@
+// Functions
+// 1. Abstract out logic in your program
+// 1.1- Take argumemnts as an input
+// 1.2- Return a value as an output
+// 1.3- think them as a independent program that is supposed to do something given in an input
+// 1.4- functions can take other functions as input - this will confuse you(callbacks)
+
+
+// 2. Create reusable code
+
+// 3. Modularize your code
+
+// 4. Test your code
+
+// 5. Clean up your code
+
+// 6. Refactor your code
+// 6.1- Refactoring is the process of changing a computer program's source code without
+// changing the program's behavior. 
+// 6.2- Refactoring is a fundamental part of software development, and is often used
+// to improve the quality of code.
+// 6.3- Refactoring is a process of improving the design of existing code without
+// changing its behavior.
+
+// function sum(a,b) {
+//     const add = a + b;
+//     return add;
+// }
+
+// const value = sum(1,2)
+// console.log(value);
